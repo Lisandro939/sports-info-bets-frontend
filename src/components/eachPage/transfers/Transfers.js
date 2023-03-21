adb00fa9d6a9b56131e49bb302843bf1d7150e44
Lisandro939 <lisandrofernandez2705@gmail.com>
2023-03-21 12:18:34 -0300

    Show a message when there is no results or transfers

--- src/components/eachPage/transfers/Transfers.js
+++ src/components/eachPage/transfers/Transfers.js
@@ -7,7 +7,7 @@ export default function Transfers({transfers}) {
     <div className='text-white px-4 flex flex-col gap-4 sm:pr-8'>
         <h1 className='text-sm'>Últimas transferencias</h1>
         {
-          transfers.length === 0 ? <h1 className='h-12'>¡Ups! No hay transferencias en el último tiempo</h1> : <></>
+          transfers.length === 0 ? <h1 className='h-24 sm:h-12'>¡Ups! No hay transferencias en el último tiempo</h1> : <></>
         }
         {
           transfers.map((transfer, index) => (
